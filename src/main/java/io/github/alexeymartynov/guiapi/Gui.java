5675fecacc1f84d80209d2e2155bec79ef99fce4
AlexeyMartynov <54399481+AlexeyMartynov@users.noreply.github.com>
2021-11-30 01:41:49 +0200

    1.2

--- src/main/java/io/github/alexeymartynov/guiapi/Gui.java
+++ src/main/java/io/github/alexeymartynov/guiapi/Gui.java
@@ -73,5 +73,5 @@ public abstract class Gui extends GuiHolder {
 			return;
 
         addToQueue(player, inventory);
-	} 
+	}
 }
--- src/main/java/io/github/alexeymartynov/guiapi/GuiHolder.java
+++ src/main/java/io/github/alexeymartynov/guiapi/GuiHolder.java
@@ -1,6 +1,7 @@
 package io.github.alexeymartynov.guiapi;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -21,4 +22,13 @@ public abstract class GuiHolder implements InventoryHolder {
 	}
 	
 	public abstract void onGuiClick(InventoryClickEvent event);
+
+	public void update()
+	{
+		for(Player player : Bukkit.getOnlinePlayers())
+		{
+			if(player.getInventory().getTitle().equals(inventory.getTitle()))
+				player.openInventory(player.getInventory());
+		}
+	}
 }
